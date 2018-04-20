5c13d108f25253fe1672d182d2113cb1b5d306a5
Elias Schildknecht <elias.schildknecht@bi-automation.ch>
2018-04-20 08:53:26 +0200

    Added card entity and card repository to represent a card (yellow, red, whatever...)

---
+++ src/main/java/com/coachingeleven/coachingsoftware/persistence/repository/CardRepository.java
@@ -0,0 +1,7 @@
+package com.coachingeleven.coachingsoftware.persistence.repository;
+
+import com.coachingeleven.coachingsoftware.persistence.entity.Card;
+
+public class CardRepository extends Repository<Card> {
+	
+}
