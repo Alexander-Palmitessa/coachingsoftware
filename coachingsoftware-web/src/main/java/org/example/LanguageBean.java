473639e519bbac6e00315bd9d6c38b7b7aec129b
Elias <elias.schildknecht@students.bfh.ch>
2018-05-14 00:01:20 +0200

    Default templating (header,pages,footer,language). Switch language.

---
+++ coachingsoftware-web/src/main/java/org/example/LanguageBean.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.io.Serializable;
+import java.util.LinkedHashMap;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.enterprise.context.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+@Named(value = "languageBean")
+@SessionScoped
+public class LanguageBean implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	private String localeCode = "blabla";
+	
+	private static Map<String,Object> countries;
+	
+	static{
+		countries = new LinkedHashMap<String,Object>();
+		countries.put("English", Locale.ENGLISH);
+		countries.put("German", Locale.GERMAN);
+	}
+
+	public Map<String, Object> getCountriesInMap() {
+		return countries;
+	}
+
+	
+	public String getLocaleCode() {
+		return localeCode;
+	}
+
+
+	public void setLocaleCode(String localeCode) {
+		this.localeCode = localeCode;
+	}
+
+	public void changeLanguage(String localeCode){
+		//loop country map to compare the locale code
+		for (Map.Entry<String, Object> entry : countries.entrySet()) {
+			if(entry.getValue().toString().equals(localeCode)){
+				FacesContext.getCurrentInstance().getViewRoot().setLocale((Locale)entry.getValue());
+			}
+		}
+	}
+}
